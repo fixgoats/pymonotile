ef631198bcccef572075091c3ab7fa9efea9b1e7
Valtýr Kári Daníelsson <vkd1@hi.is>
2025-08-18 13:04:14 +0000

    fékk ferningsgrindina til að virka

--- bleh.py
+++ bleh.py
@@ -1,15 +1,1 @@
-import numpy as np
-
-vertices = np.loadtxt("smallermonotilepoints.txt")
-N = np.shape(vertices)[0]
-b = np.zeros((N, N))
-for i in range(N):
-    for j in range(i):
-        d = np.linalg.norm(vertices[i, :])
-        if d < 3.9:
-            b[i, j] = np.exp(-d)
-            b[j, i] = np.exp(-d)
-
-eigvals, eigvec = np.linalg.eigh(b)
-np.savetxt("frequencies.txt", eigvals)
-np.savetxt("eigvecs.txt", eigvec)
+impot numpy as np
--- qutiptest.py
+++ qutiptest.py
@@ -1,6 +1,6 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from qutip import basis, expect
 import numpy as np
-from qutip import basis
 
 N = 20
 
@@ -8,28 +8,28 @@ kx = np.linspace(0, np.pi, N)
 
 kxv, kyv = np.meshgrid(kx, kx)
 
-H = sum(
+h = sum(
     [
-        basis(N * N, j * N + i) * basis(N * N, (j + 1) * N + i).dag()
+        -basis(N * N, j * N + i) * basis(N * N, (j + 1) * N + i).dag()
         for i in range(N)
         for j in range(N - 1)
     ]
 )
-H += sum(
+h += sum(
     [
-        basis(N * N, j * N + i) * basis(N * N, (j - 1) * N + i).dag()
+        -basis(N * N, j * N + i) * basis(N * N, (j - 1) * N + i).dag()
         for i in range(N)
         for j in range(1, N)
     ]
 )
-H += sum(
+h += sum(
     [
         -basis(N * N, j * N + i) * basis(N * N, j * N + i + 1).dag()
         for i in range(N - 1)
         for j in range(N)
     ]
 )
-H += sum(
+h += sum(
     [
         -basis(N * N, j * N + i) * basis(N * N, j * N + i - 1).dag()
         for i in range(1, N)
@@ -37,36 +37,48 @@ H += sum(
     ]
 )
 
-evals, ekets = H.eigenstates()
-reordered = np.zeros((N, N))
-idx = 0
-for i in range(N):
-    for j in range(i):
-        for k in range(i - j):
-            reordered[i, j] = evals[idx]
-            idx += 1
+# evals, ekets = H.eigenstates()
+# reordered = np.zeros((N, N))
+# idx = 0
+# for i in range(N):
+#     for j in range(i):
+#         for k in range(i - j):
+#             reordered[i, j] = evals[idx]
+#             idx += 1
 
 
 dk = np.pi / N
 bleh = np.arange(0, np.pi, dk)
 r_basis = [basis(N * N, j * N + i) for i in range(N) for j in range(N)]
-k_basis = [
-    (1 / N)
-    * sum(
+# k_basis = np.array([[
+#     (1 / N)
+#     * 
+#     )
+#     for kx in bleh]
+#     for ky in bleh
+# ])
+
+
+es = np.array([
+              [
+              expect(h, sum(
         [
             np.exp(1j * (kx * i + ky * j)) * basis(N * N, j * N + i)
             for i in range(N)
             for j in range(N)
-        ]
-    )
-    for kx in bleh
-    for ky in bleh
-]
+        ])/ N)  for kx in bleh]
+              for ky in bleh]
+              )
+print(es)
 
-np.savetxt("squarefreq.txt", evals)
-np.savetxt("squareigvecs.txt", evals)
+# np.savetxt("squarefreq.txt", evals)
+# np.savetxt("squareigvecs.txt", evals)
 
-# fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-# ax.plot_surface(kxv, kyv, reordered)
+analytical = -2 * (np.cos(kxv) + np.cos(kyv))
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+ax.plot_surface(kxv, kyv, es)
+ax.plot_surface(kxv, kyv, analytical)
 #
-# fig.savefig("square.pdf")
+plt.show()
+fig.savefig("square.pdf")
