8b6f6f6b0ed34cbb8002573829d4516c841044a6
fixgoats <fixgoats@mGLaDOS>
2025-08-28 14:55:12 +0000

    did some work on the fibonacci chain

--- fibonacciapproximants.py
+++ fibonacciapproximants.py
@@ -1,15 +1,51 @@
 from pythtb import tb_model
 import matplotlib.pyplot as plt
+from functools import cache
+from numba import njit, typed
 
-lat = [[8]]
-orb = [[0], [2], [3], [5], [7]]
+
+@cache
+def fibwordgen(n):
+    if n == 0:
+        return [2]
+    if n == 1:
+        return [2, 1]
+    return fibwordgen(n - 1) + fibwordgen(n - 2)
+
+
+@njit
+def cumsum(l):
+    s = None
+    retl = [0] * len(l)
+    for i, e in enumerate(l):
+        if i == 0:
+            s = e
+            retl[0] = e
+        else:
+            s += e
+            retl[i] = s
+    return retl
+
+
+@njit
+def wrapnums(l):
+    return [[e] for e in l]
+
+
+ninthgen = fibwordgen(14)
+typed_ninthgen = typed.List(ninthgen)
+print(cumsum(typed_ninthgen))
+
+lat = [[sum(ninthgen)]]
+orb = wrapnums(cumsum(typed_ninthgen))
 
 chain = tb_model(1, 1, lat, orb)
-chain.set_hop(1, 0, 1, [0])
-chain.set_hop(2, 1, 2, [0])
-chain.set_hop(1, 2, 3, [0])
-chain.set_hop(1, 3, 4, [0])
-chain.set_hop(2, 4, 0, [1])
+for i, e in enumerate(ninthgen):
+    t = 1 if e == 2 else 2
+    if i == len(ninthgen) - 1:
+        chain.set_hop(t, i, 0, [1])
+    else:
+        chain.set_hop(t, i, i + 1, [0])
 
 chain.display()
 
@@ -17,21 +53,12 @@ chain.display()
 k_label = ("$0$", r"$\pi$", r"$2\pi$")
 
 evals = chain.solve_all(k_vec)
-simplelat = [[1.0]]
-simpleorb = [[0.0]]
-my_model = tb_model(1, 1, lat, orb)
-my_model.set_hop(1.0, 0, 0, [1])
-
-# define a path in k-space
-(simplek_vec, simplek_dist, simplek_node) = my_model.k_path("full", 100)
-
-# solve model
-simple_evals = my_model.solve_all(k_vec)
-
 
 fig, ax = plt.subplots()
-ax.plot(k_dist, evals[0])
-ax.plot(simplek_dist, simple_evals[0])
+fig.set_size_inches(6, 20)
+print(len(evals))
+for e in evals:
+    ax.plot(k_dist, e)
 ax.set_title("1D chain band structure")
 ax.set_xlabel("Path in k-space")
 ax.set_ylabel("Band energy")
@@ -41,4 +68,4 @@ ax.set_xlim(k_node[0], k_node[-1])
 for n in range(len(k_node)):
     ax.axvline(x=k_node[n], linewidth=0.5, color="k")
 fig.tight_layout()
-fig.savefig("fibonacchichain.pdf")
+fig.savefig("fibonaccichain.pdf")
