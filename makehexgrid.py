49fab886fc37eb747bf8a6cf241d35c2078953e3
Valtýr Kári Daníelsson <vkd1@hi.is>
2025-11-12 01:27:55 +0000

    exact dispersion for monotile superlattice

--- makehexgrid.py
+++ makehexgrid.py
@@ -10,13 +10,19 @@ a2 = np.array([a/2, np.sqrt(3) * a/2])
 o1 = np.array([0, -a_cc / 2])
 o2 = np.array([0, a_cc / 2])
 grid = np.ndarray((0,2))
+<<<<<<< HEAD
 n = 20
 for i in range(n):
     for j in range(n):
+=======
+for i in range(71):
+    for j in range(71):
+>>>>>>> 1668691 (exact dispersion for monotile superlattice)
         p1 = i*a1 + j*a2 + o1
         p2 = i*a1 + j*a2 + o2
         grid = np.vstack((grid, [p1], [p2]))
 
+<<<<<<< HEAD
 midpoint = n/2*a1 + n/2*a2
 mask = np.linalg.norm(grid - midpoint, axis=1) < n*a/4
 print(mask)
@@ -28,3 +34,6 @@ ax.scatter(grid[:, 0], grid[:, 1])
 plt.show()
 
 np.savetxt("smallcirchexgrid.txt", grid)
+=======
+np.savetxt("largerhexgrid.txt", grid)
+>>>>>>> 1668691 (exact dispersion for monotile superlattice)
--- sdfsections.py
+++ sdfsections.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
@@ -23,3 +24,52 @@ im = ax.imshow(dset, extent=(bounds[0], bounds[2], bounds[4], bounds[5]), interp
 cb = plt.colorbar(im)
 plt.show()
 fig.savefig("largemonotiledisp.pdf")
+=======
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = h5py.File("graphenesection.h5", "r")
+dset = f["section"][:,:]
+bounds = f["section_bounds"][:]
+# dset = f["sdf"]
+# bounds = f["sdf_bounds"]
+
+# zerokysection = dset[:,0,:]
+# bounds = bounds[:]
+
+# fig, ax = plt.subplots()
+# im1 = ax.imshow(zerokysection.T, extent=[bounds[0], bounds[1], bounds[4], bounds[5]], origin="lower", interpolation="none", aspect='auto')
+# cb = plt.colorbar(im1)
+# 
+# ax.set_xlabel("$k_x")
+# ax.set_ylabel("$E$")
+# ax.set_title("Dispersion relation ($k_y = 0$)")
+# 
+# fig.savefig("disprelation.pdf")
+# plt.show()
+# plt.cla()
+fig, ax = plt.subplots()
+# midesection = dset[:,:,99]
+im2 = ax.imshow(dset, extent=[bounds[0], bounds[1], bounds[2], bounds[3]], aspect="auto", origin="lower", interpolation="none")
+cb = plt.colorbar(im2)
+
+ax.set_xlabel("$k_x$")
+ax.set_ylabel("$k_y$")
+# de = (bounds[5] - bounds[4]) / 100
+# mide = bounds[4] + 99 * de
+ax.set_title(f"Spectral density at $E = {bounds[4]}$")
+fig.savefig("grapheneesection.pdf")
+plt.show()
+# plt.cla()
+# fig, ax = plt.subplots()
+# dos = np.sum(dset[:,:,:], axis=(0, 1))
+# es = np.linspace(bounds[4], bounds[5], 200, endpoint=False)
+# 
+# ax.plot(es, dos)
+# ax.set_xlabel("$E$")
+# ax.set_ylabel("$\\rho(E)$")
+# ax.set_title("Density of states")
+# plt.show()
+# fig.savefig("densityofstates.pdf")
+>>>>>>> 1668691 (exact dispersion for monotile superlattice)
